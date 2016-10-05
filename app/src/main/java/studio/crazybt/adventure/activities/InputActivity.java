fdad0c3fa94ae7b59bf99e1b056e0cdc9d2828d3
BruceleeThanh <nguyentrungthanh602@gmail.com>
2016-10-05 17:45:20 +0700

    add Create Status

---
+++ app/src/main/java/studio/crazybt/adventure/activities/InputActivity.java
@@ -0,0 +1,83 @@
+package studio.crazybt.adventure.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import studio.crazybt.adventure.R;
+import studio.crazybt.adventure.fragments.CreateStatusFragment;
+import studio.crazybt.adventure.helpers.FragmentController;
+
+public class InputActivity extends AppCompatActivity {
+
+    private static int typeShow = 0;
+    private FragmentController fragmentController;
+
+    @BindView(R.id.tbInput)
+    Toolbar tbInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+        ButterKnife.bind(this);
+        setSupportActionBar(tbInput);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Intent intent = getIntent();
+        typeShow = intent.getIntExtra("TYPE_SHOW", typeShow);
+        switch (typeShow){
+            case 1:
+                this.showCreateStatus();
+                break;
+            case 2:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showCreateStatus(){
+        getSupportActionBar().setTitle(getResources().getString(R.string.title_tb_create_status));
+        fragmentController = new FragmentController(this);
+        fragmentController.addFragment_BackStack(R.id.rlInput, new CreateStatusFragment());
+        fragmentController.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getSupportFragmentManager().getBackStackEntryCount() == 1){
+            finish();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu_input, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            case R.id.itemPost:
+                Toast.makeText(this, "Post done", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/studio/crazybt/adventure/fragments/TabNewfeedHomePageFragment.java
+++ app/src/main/java/studio/crazybt/adventure/fragments/TabNewfeedHomePageFragment.java
@@ -1,5 +1,6 @@
 package studio.crazybt.adventure.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,37 +10,57 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.clans.fab.FloatingActionButton;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import studio.crazybt.adventure.R;
+import studio.crazybt.adventure.activities.InputActivity;
+import studio.crazybt.adventure.activities.StatusActivity;
 import studio.crazybt.adventure.adapters.NewfeedListAdapter;
 
 /**
  * Created by Brucelee Thanh on 11/09/2016.
  */
-public class TabNewfeedHomePageFragment extends Fragment{
+public class TabNewfeedHomePageFragment extends Fragment implements View.OnClickListener {
 
+    private static final int INSERT_STATUS = 1;
     private View rootView;
-    @BindView(R.id.rvNewfeed)
-    RecyclerView rvNewfeed;
     private LinearLayoutManager llmNewFeed;
     private NewfeedListAdapter nlaNewfeed;
+
+    @BindView(R.id.rvNewfeed)
+    RecyclerView rvNewfeed;
+    @BindView(R.id.fabCreateStatus)
+    FloatingActionButton fabCreateStatus;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if(rootView == null){
+        if (rootView == null) {
             rootView = inflater.inflate(R.layout.fragment_tab_newfeed_home_page, container, false);
             ButterKnife.bind(this, rootView);
+            fabCreateStatus.setOnClickListener(this);
             this.initNewFeedList();
         }
         return rootView;
     }
 
-    public void initNewFeedList(){
+    public void initNewFeedList() {
         llmNewFeed = new LinearLayoutManager(getContext());
         rvNewfeed.setLayoutManager(llmNewFeed);
         nlaNewfeed = new NewfeedListAdapter(this.getContext());
         rvNewfeed.setAdapter(nlaNewfeed);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fabCreateStatus:
+                Intent intent = new Intent(getActivity(), InputActivity.class);
+                intent.putExtra("TYPE_SHOW", INSERT_STATUS);
+                startActivity(intent);
+                break;
+        }
     }
 }
